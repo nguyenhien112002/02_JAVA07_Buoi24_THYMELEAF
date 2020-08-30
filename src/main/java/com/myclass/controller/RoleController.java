5cda7f62465ece551d82cdb0cdfa89aae8f2061f
nguyenhien112002 <nguyenhien112002@yahoo.com>
2020-08-30 14:20:28 +0700

    2

--- src/main/java/com/myclass/controller/RoleController.java
+++ src/main/java/com/myclass/controller/RoleController.java
@@ -1,10 +1,12 @@
 package com.myclass.controller;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@Component
+//@Component
+@Controller
 @RequestMapping("role")
 public class RoleController {
     
